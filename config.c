7eae8779475b9cc4f3ecab47ff326102cfd816ca
Alexey Sheshukov <alexeyshesh@MacBook-Pro-Alexey-4.local>
2019-11-21 23:41:33 +0300

    second commit

---
+++ config.c
@@ -0,0 +1,74 @@
+#include "config.h"
+
+void fatal_error(error_code code, ...) {
+
+    va_list msg; /* pointer to possible parameter */
+    va_start(msg, code); /* places msg to parameter after code */ 
+
+    printf(ANSI_COLOR_RED "\nFatal error: ");
+    switch (code) {
+        case HEAP_OVERFLOW: {
+            printf("heap overflow\n");
+        }
+        break;
+        case FILE_OPEN_ERROR: {
+            printf("can not open file\n");
+        }
+        break;
+        case NO_FILE: {
+            printf("file doesn't exist\n");
+        }
+        break;
+        case BAN_CH: {
+            printf("forbidden character '%c' used\n", va_arg(msg, int));
+        }
+        break;
+        case INV_PAR: {
+            printf("invalid amount of parameters\n");
+        }
+        break;
+        case PROC: {
+            printf("can not start process\n");
+        }
+        break;
+        case CMD_NOT_FOUND: {
+            printf("command not found\n");
+        }
+        break;
+        case CUR_DIR_ERR: {
+            printf("can not get current directory address\n");
+        }
+        break;
+        case PIPE_ERR: {
+            printf("can not continue conveyor execution\n");
+        }
+        break;
+    }
+    printf(ANSI_COLOR_RESET);
+    exit(1);
+}
+
+void print_cwd() {
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("%s", cwd);
+    } else {
+        fatal_error(CUR_DIR_ERR);
+    }
+}
+
+void clear_screen() {
+    int child;
+    if ((child = fork()) < 0) {
+        return;
+    } else if (child == 0) {
+        char *argv[2];
+        argv[0] = "clear";
+        argv[1] = NULL;
+        execvp("clear", argv);
+        exit(0);
+    } else {
+        waitpid(child, NULL, 0);
+        return;
+    }
+}
---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <signal.h>
+
+#include "config.h"
+#include "tokenlist.h"
+#include "syntax.h"
+
+int main(int argc, char **argv) {
+    signal(SIGINT, SIG_IGN);
+    int in_desc = 0; /* input file descriptor */
+    if (argc == 1) {
+        in_desc = 0;
+    } else if (argc == 2) {
+        if ((in_desc = open(argv[1], O_RDONLY)) != -1) {
+            dup2(in_desc, 0);
+            close(in_desc);
+        } else
+            fatal_error(FILE_OPEN_ERROR);
+    } else 
+        fatal_error(INV_PAR);
+    
+    clear_screen();
+    token_list *q = NULL;
+    
+    while (1) {
+        if (in_desc == 0) {
+            print_cwd();
+            printf(": ");
+        }
+        
+        q = get_token_list();
+        if (q != NULL) {
+            syntax_analysis(q);
+            clear_token_list(q);
+        }
+    }
+    return 0;
+}
